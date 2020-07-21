d190990914fc8253e15f2fbde1ec8df9f3b3f556
Amber Olsen <amcmains@ucsd.edu>
2020-07-21 15:33:49 -0700

    first commit

---
+++ app/src/main/java/com/hfad/ival_revisited/MainActivity.java
@@ -0,0 +1,174 @@
+package com.hfad.ival_revisited;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.usage.NetworkStats;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.sip.SipSession;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements RecognitionListener {
+
+    private Button btnSwitch;
+    private static final int REQUEST_RECORD_PERMISSION = 100;
+    private TextView textOutput;
+    SpeechRecognizer speech;
+    private Intent recognizerIntent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSwitch = findViewById(R.id.button);
+        textOutput= (TextView) findViewById(R.id.textView);
+
+        speech = SpeechRecognizer.createSpeechRecognizer(this);
+        Log.i("speech", "onCreate: " + SpeechRecognizer.isRecognitionAvailable(this));
+        speech.setRecognitionListener(this);
+        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,"US-en");
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        //recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
+        ActivityCompat.requestPermissions (MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_PERMISSION);
+        speech.startListening(recognizerIntent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0]== PackageManager.PERMISSION_GRANTED) {
+                    speech.startListening(recognizerIntent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied!", Toast .LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (speech != null) {
+            speech.destroy();
+            Log.i("destroy", "destroy");
+        }
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle arg0) {
+        Log.i("ready", "onReadyForSpeech");
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onRmsChanged(float rmsdB) {
+
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+        Log.i("end", "onEndOfSpeech: ");
+    }
+
+    @Override
+    public void onError(int error) {
+        String errorMessage = getErrorText(error);
+        textOutput.setText(errorMessage);
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        Log.i("results", "onResults: ");
+        ArrayList<String> matches = results .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        String text = "";
+        for (String result : matches)
+            text = result + "\n";
+        Log.i("results", "onResults: " + text);
+        textOutput.setText(text);
+    }
+
+    @Override
+    public void onPartialResults(Bundle partialResults) {
+
+    }
+
+    @Override
+    public void onEvent(int eventType, Bundle params) {
+
+    }
+
+
+public static String getErrorText(int errorCode) {
+    String message;
+    switch (errorCode) {
+        case SpeechRecognizer.ERROR_AUDIO:
+            message = "Audio recording error";
+            break;
+        case SpeechRecognizer.ERROR_CLIENT:
+            message = "Client side error";
+            break;
+        case
+                SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+            message = "Insufficient permissions";
+            break;
+        case SpeechRecognizer.ERROR_NETWORK:
+            message = "Network error";
+            break;
+        case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+            message = "Network timeout";
+            break;
+        case SpeechRecognizer.ERROR_NO_MATCH:
+            message = "No match";
+            break;
+        case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+            message = "RecognitionService busy";
+            break;
+        case SpeechRecognizer.ERROR_SERVER:
+            message = "error from server";
+            break;
+        case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+            message = "No speech input";
+            break;
+        default:
+            message = "Didn't understand, please try again.";
+            break;
+    }
+    return message;
+}
+
+}
+
+
+
