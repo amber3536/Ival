11fcb6dc99fed7de59e8d8eed9e7baa41e20e3a8
Amber Olsen <amcmains@ucsd.edu>
2020-08-09 14:30:06 -0700

    working on position buttons

--- app/src/main/java/com/hfad/ival_revisited/PositionFragment.java
+++ app/src/main/java/com/hfad/ival_revisited/PositionFragment.java
@@ -4,14 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class PositionFragment extends Fragment {
     View view;
+    Button leftBtn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_position, container, false);
+        leftBtn = view.findViewById(R.id.shot_left);
+
+        leftBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "left", Toast.LENGTH_SHORT).show();
+            }
+        });
 // get the reference of Button
 //        firstButton = (Button) view.findViewById(R.id.firstButton);
 //// perform setOnClickListener on first Button
